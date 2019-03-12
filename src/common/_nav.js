7486b4a7c7984073131086447c69150e6fe7f9e4
trainee31 <psspl.trainee31@outlook.com>
2019-03-12 16:19:02 +0530

    merge

--- src/common/_nav.js
+++ src/common/_nav.js
@@ -31,80 +31,6 @@ export default {
         },
 
         {
-<<<<<<< HEAD
-            name: 'Administrator',
-            icon: 'fa fa-graduation-cap',
-            id: 'lnkSettings',
-            children: [{
-                name: 'KRA',
-                url: '/kra',
-                icon: 'fa fa-area-chart',
-            },
-
-            {
-                name: 'Role',
-                url: '/role',
-                icon: 'fa fa-user-secret',
-            },
-            {
-                name: 'Quarter',
-                url: '/quater',
-                icon: 'fa fab fa-quora',
-            },
-
-            {
-                name: 'User Management',
-                url: '/user-management',
-                icon: 'fa fa-user-o'
-            },
-            {
-                name: "Department",
-                url: '/Department',
-                icon: 'fa fa-building-o'
-            },
-            {
-                name: 'KPI',
-                url: '/kpi',
-                icon: 'fa fa-key',
-            },
-            {
-                name: 'Complexity Master',
-                url: '/complexity-master',
-                icon: 'fa fa-snowflake-o',
-            },
-            {
-                name: 'Template',
-                url: '/templateList',
-                icon: 'fa fa-clipboard',
-            },
-
-            {
-                name: 'Scale Set',
-                url: '/scale-set',
-                icon: 'fa fa-balance-scale',
-            },
-            {
-                name: 'Designation',
-                url: '/designation',
-                icon: 'fa fa-id-card-o',
-            },
-            {
-                name: 'Projects',
-                url: '/projects',
-                icon: 'fa fa-sitemap',
-            },
-            {
-                name: 'Assign-Template',
-                url: '/Template',
-                icon: 'fa fa-tasks',
-            },
-
-            ]
-        }
-
-]
-};
-=======
             name: 'KRA',
             url: '/kra',
             icon: 'fa fa-area-chart',
@@ -169,4 +95,3 @@ export default {
     ]
 }
 
->>>>>>> bac39bbcf8e4b1924ae3dfccf96d14eac01a11c5
