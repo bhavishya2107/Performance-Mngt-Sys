f32294df50cbbb26def158e972d8109e8fce649e
Nikita <psspl.trainee29@outlook.com>
2019-03-15 11:04:53 +0530

    common changes in user access rights in both app.js and nav.js

--- src/common/_nav.js
+++ src/common/_nav.js
@@ -1,6 +1,6 @@
 var menuItems = [];
-if (localStorage.getItem('roleName') =="HR") {
-      menuItems.push(
+if (localStorage.getItem('roleName') == "HR") {
+    menuItems.push(
         {
             name: 'My Team',
             url: '/myteam',
@@ -69,9 +69,9 @@ if (localStorage.getItem('roleName') =="HR") {
             icon: 'fa fa-sitemap',
         }
 
-    
+
     );
-} else if (localStorage.getItem('roleName') =="TPM") {
+} else if (localStorage.getItem('roleName') == "TPM") {
     menuItems.push(
         {
             name: 'My KRA',
@@ -85,81 +85,14 @@ if (localStorage.getItem('roleName') =="HR") {
         },
     );
 }
-else{
-menuItems.push(
-    {
-        name: 'My KRA',
-        url: '/dashboard',
-        icon: 'fa fa-dashboard',
-    },
-    {
-        name: 'My Team',
-        url: '/myteam',
-        icon: 'fa fa-users',
-    },
-    {
-        name: 'KRA',
-        url: '/kra',
-        icon: 'fa fa-area-chart',
-    },
-
-    {
-        name: 'Role',
-        url: '/role',
-        icon: 'fa fa-user-secret',
-    },
-    {
-        name: 'Quarter',
-        url: '/quater',
-        icon: 'fa fab fa-quora',
-    },
-
-    {
-        name: 'User Management',
-        url: '/user-management',
-        icon: 'fa fa-user-o'
-    },
-    {
-        name: "Department",
-        url: '/Department',
-        icon: 'fa fa-building-o'
-    },
-    {
-        name: 'KPI',
-        url: '/kpi',
-        icon: 'fa fa-key',
-    },
-    {
-        name: 'Complexity Master',
-        url: '/complexity-master',
-        icon: 'fa fa-snowflake-o',
-    },
-    {
-        name: 'Template',
-        url: '/templateList',
-        icon: 'fa fa-clipboard',
-    },
-    {
-        name: 'Scale Set',
-        url: '/scale-set',
-        icon: 'fa fa-balance-scale',
-    },
-    {
-        name: 'Designation',
-        url: '/designation',
-        icon: 'fa fa-id-card-o',
-    },
-    {
-        name: 'Projects',
-        url: '/projects',
-        icon: 'fa fa-sitemap',
-    },
-    {
-        name: 'Assign-Template',
-        url: '/Assign-Template',
-        icon: 'fa fa-tasks',
-    }
-)
+else {
+    menuItems.push(
+        {
+            name: 'My KRA',
+            url: '/dashboard',
+            icon: 'fa fa-dashboard',
+        },
+    )
 }
 export default {
     items: menuItems,
