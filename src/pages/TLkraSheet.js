662c4957e79c21dca6a9549d6cebb0c5f55cd0d1
trainee31 <psspl.trainee31@outlook.com>
2019-03-29 14:47:38 +0530

    code formatting,removed debugger

--- src/pages/TLkraSheet.js
+++ src/pages/TLkraSheet.js
@@ -48,7 +48,6 @@ class TLKraSheet extends Component {
 
      //add Employee Comment
   saveReviewerComment(data) {
-    debugger
             var employeeComment =
             {
                 "reviewerComment": data.reviewerComment,
@@ -70,7 +69,6 @@ class TLKraSheet extends Component {
 
    //get reviewerComment for update 
    apiForGetReviewerComment() {
-    debugger
           const apiForGetrevComment = environment.apiUrl + moduleUrls.Template_assignment_master + '/' + `${this.state.assignId}`
           return $.ajax({
               url: apiForGetrevComment,
